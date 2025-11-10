db39e56b742ad85972d19edc91978c2b9f2d2efc
PIUS <hwangpiusjoon@gmail.com>
2025-11-10 18:46:52 +0900

    Create ASSERT.cpp

---
+++ GameEngineArchitecture/ASSERT.cpp
@@ -0,0 +1,29 @@
+//Static_ASSERT 11버전 이하
+#include <iostream>
+
+#ifdef __cplusplus
+#if __cplusplus>=201103L
+#define STATIC_ASSERT(expr) static_assert(expr, "static assert failed:" #expr)
+#else
+template<bool> class TStaticAssert;
+template<> class TStaticAssert<true>{};
+
+#define STATIC_ASSERT(expr)
+enum
+{
+    ASSERT_GLUE(g_assert_fail_,__LINE__) = sizeof(TStaticAssert<!!(expr)>)
+}
+#endif
+#endif
+
+//C++ 2011이하면 다른거 활용
+
+
+int main() {
+    // Write C++ code here
+    std::cout << "Try programiz.pro";
+
+    return 0;
+}
+
+
